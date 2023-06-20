aa2cb0f50d19d217731d6fb2fb5a9479e76fa627
Jonas Nygren <jonas_nygren30@hotmail.com>
2023-06-21 00:25:22 +0200

    Update Website Availability Check.ps1
    Added Commets

--- Website Availability Check.ps1
+++ Website Availability Check.ps1
@@ -1,11 +1,16 @@
-﻿$websites = "https://www.microsoft.com", "https://www.google.com"
+$websites = "https://www.microsoft.com", "https://www.google.com"
 
+# Loop through each website in the array
 foreach ($website in $websites) {
+    # Send an HTTP request to the current website and store the response
     $response = Invoke-WebRequest -Uri $website -UseBasicParsing
 
+    # Check the status code of the response
     if ($response.StatusCode -eq 200) {
+        # If the status code is 200 (OK), print that the website is online
         Write-Host "Website $($response.BaseResponse.ResponseUri) is online"
     } else {
+        # If the status code is not 200, print that the website is offline
         Write-Host "Website $($response.BaseResponse.ResponseUri) is offline"
     }
 }
