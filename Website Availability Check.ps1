706f3c90c95569d1fca9592332cb87889642474e
Jonas Nygren <jonas_nygren30@hotmail.com>
2023-06-21 00:15:24 +0200

    Website Availability Check

---
+++ Website Availability Check.ps1
@@ -0,0 +1,11 @@
+﻿$websites = "https://www.microsoft.com", "https://www.google.com"
+
+foreach ($website in $websites) {
+    $response = Invoke-WebRequest -Uri $website -UseBasicParsing
+
+    if ($response.StatusCode -eq 200) {
+        Write-Host "Website $($response.BaseResponse.ResponseUri) is online"
+    } else {
+        Write-Host "Website $($response.BaseResponse.ResponseUri) is offline"
+    }
+}
